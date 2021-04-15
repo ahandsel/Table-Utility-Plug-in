a69f76db6e5aee324376285b33c8be91c3a73528
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2021-04-15 20:16:27 +0900

    Starting Point Code is tested & done ✅

---
+++ 1_Starting_Point/src/js/config.js
@@ -0,0 +1,61 @@
+/*
+MIT License Copyright (c) 2018 Cybozu
+https://github.com/kintone/SAMPLE-Table-numbering-plug-in/blob/master/LICENSE
+*/
+
+jQuery.noConflict();
+(function ($, PLUGIN_ID) {
+  'use strict';
+  // Get configuration settings
+
+  var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
+  var $form = $('.js-submit-settings');
+  var $cancelButton = $('.js-cancel-button');
+  var $number = $('select[name="js-select-number-field"]');
+
+  function escapeHtml(htmlstr) {
+    return htmlstr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+  }
+
+  function setDropDown() {
+    // Retrieve field information, then set drop-down
+    return KintoneConfigHelper.getFields('NUMBER')
+      .then(function (resp) {
+        resp.forEach(function (field) {
+          if (field.subtableCode) {
+            var $option = $('<option>');
+            $option.attr('value', field.subtableCode + ',' + field.code); // Set table code and number field code
+            $option.text(escapeHtml(field.label));
+            $number.append($option.clone());
+          }
+        });
+        // Set default values
+        $number.val(CONF.table + ',' + CONF.number);
+      }, function () {
+        return alert('Failed to retrieve field(s) information');
+      });
+  }
+  $(document).ready(function () {
+    // Set drop-down list
+    setDropDown();
+    // Set input values when 'Save' button is clicked
+    $form.on('submit', function (e) {
+      var config = [];
+      var number = $number.val();
+      e.preventDefault();
+
+      config.table = number.split(',')[0]; // Set table field code
+      config.number = number.split(',')[1]; // Set number field code
+
+      kintone.plugin.app.setConfig(config, function () {
+        alert('The plug-in settings have been saved. Please update the app!');
+        window.location.href = '/k/admin/app/flow?app=' + kintone.app.getId();
+      });
+    });
+    // Process when 'Cancel' is clicked
+    $cancelButton.on('click', function () {
+      window.location.href = '/k/admin/app/' + kintone.app.getId() + '/plugin/';
+    });
+  });
+})(jQuery, kintone.$PLUGIN_ID);
---
+++ 1_Starting_Point/src/js/desktop.js
@@ -0,0 +1,54 @@
+/*
+ * This sample plug-in automatically adds row numbers to table rows each time the record is saved.
+ * Copyright (c) 2018 Cybozu
+ *
+ * Licensed under the MIT License
+ */
+(function (PLUGIN_ID) {
+  'use strict';
+
+  var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID); // Get plug-in configuration settings
+  var TABLEFIELD, NUMBERFIELD, disableEvents;
+  // Disable number fields in table at these events
+  var numberEvents, row;
+  // Get each setting
+  if (!CONFIG) {
+    return false;
+  }
+
+  TABLEFIELD = CONFIG.table; // Field code of the table
+  NUMBERFIELD = CONFIG.number; // Field code of number field in the table
+  disableEvents = [
+    'app.record.edit.show',
+    'app.record.create.show',
+    'app.record.edit.change.' + TABLEFIELD,
+    'app.record.create.change.' + TABLEFIELD
+  ];
+
+  kintone.events.on(disableEvents, function (event) {
+    var record = event.record;
+    // Disable number fields in table rows
+    var count = record[TABLEFIELD].value.length;
+    for (row = 0; row < count; row++) {
+      record[TABLEFIELD].value[row].value[NUMBERFIELD].disabled = true;
+    }
+    return event;
+  });
+
+  // Number table rows at these events
+  numberEvents = [
+    'app.record.create.submit',
+    'app.record.edit.submit'
+  ];
+  kintone.events.on(numberEvents, function (event) {
+    var record = event.record;
+
+    // Auto-number the table rows
+    var count = record[TABLEFIELD].value.length;
+    for (row = 0; row < count; row++) {
+      record[TABLEFIELD].value[row].value[NUMBERFIELD].value = row + 1;
+    }
+
+    return event;
+  });
+})(kintone.$PLUGIN_ID);
